678787fd2cbf7efea06e58b513d33e4a4e0c1e1d
programmingnoob53 <wrld999.010@gmail.com>
2026-08-13 21:07:43 +0200

    Added an icon

--- src/smallCalculator/Main.java
+++ src/smallCalculator/Main.java
@@ -2,14 +2,18 @@ package smallCalculator;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application{
+	
+	private Image icon = new Image("assets/icon.jpg");
 
 	@Override
 	public void start(Stage stage) throws Exception {
 		GUIPane pane = new GUIPane();
 		Scene scene = new Scene(pane, 500, 500);
+		stage.getIcons().add(icon);
 		stage.setScene(scene);
 		stage.setTitle("Calculator");
 		stage.setResizable(false);
